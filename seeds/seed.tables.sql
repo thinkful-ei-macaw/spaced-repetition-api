44ffb3cad05eb3049db06a56085c6a3c18b101b8
Sara R Mills <SaraRMills@gmail.com>
2020-05-18 10:03:39 -0400

    adding working branch

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -9,8 +9,8 @@ INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
   (
     1,
-    'admin',
-    'Dunder Mifflin Admin',
+    'test',
+    'test',
     -- password = "pass"
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
