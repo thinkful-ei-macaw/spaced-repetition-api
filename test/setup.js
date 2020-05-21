7308ed159e1fd23ed925f367c9ce70df39ad96f3
Sara R Mills <SaraRMills@gmail.com>
2020-05-21 18:51:55 -0400

    setup and unskip tests

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 process.env.TZ = 'UCT'
 process.env.NODE_ENV = 'test'
-process.env.JWT_SECRET = 'test-jwt-secret'
+process.env.JWT_SECRET = 'lkj689^&jKhghhG87%%7khg9)t'
 process.env.JWT_EXPIRY = '3m'
 
 require('dotenv').config()
